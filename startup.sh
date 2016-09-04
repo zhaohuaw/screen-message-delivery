41523787b2aa8ce52b995559785d61e44ff29879
Jiang haiyun <jiang.haiyun@gmail.com>
2016-09-04 15:35:23 +0800

    fix startup

--- startup.sh
+++ startup.sh
@@ -7,16 +7,22 @@ export FLASK_DEBUG=False
 flask run &
 sleep 5s # waiting flask
 
-#-o /dev/null throws away the usual output
-#--silent throws away the progress meter
-#--head makes a HEAD HTTP request, instead of GET
-#--write-out '%{http_code}\n' prints the required status code
-status = $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://127.0.0.1:5000/)
-
-while [$status != 200]
+echo out while
+while [ true ]
 do
-sleep 5s # waiting flask
-status = $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://127.0.0.1:5000/)
+    #-o /dev/null throws away the usual output
+    #--silent throws away the progress meter
+    #--head makes a HEAD HTTP request, instead of GET
+    #--write-out '%{http_code}\n' prints the required status code
+    status=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://127.0.0.1:5000/)
+    if [ "$status" != 200 ]
+    then
+        echo "waiting..."
+        sleep 5s # waiting flask
+        status=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://127.0.0.1:5000/)
+    else
+        break
+    fi
 done
 
 google-chrome --kiosk http://127.0.0.1:5000/detail/
